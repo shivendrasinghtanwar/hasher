b0c71e3e1e1e5b3d73638423828536123374fe99
sunnysinghtanwar <sunnysinghtanwar2@gmail.com>
2019-07-11 20:01:06 +0530

    Add top bar with color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
