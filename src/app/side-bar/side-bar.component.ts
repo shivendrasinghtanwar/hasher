5214027bd5f7b9c2b63d7a97298cfac97cef5c54
Shivendra <sunnysinghtanwar2@gmail.com>
2019-07-12 14:05:23 +0530

    ADD main view window with some mock content

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -8,17 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SideBarComponent implements OnInit {
 
   constructor() { }
-  menuItemList = [
-    {
-      'title':'SHA-256'
-    },
-    {
-      'title':'MD-5'
-    },
-    {
-      'title':'SHA-64'
-    }
-  ]
+  
+  @Input() menuItemList
   ngOnInit() {
     
   }
